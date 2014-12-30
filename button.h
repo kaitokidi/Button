4f6ddf13a95f083912d09f5ee5c93375b27901e0
kaitokidi <genis.bayona@est.fib.upc.edu>
2014-12-30 02:04:20 +0100

    Update button.h

--- button.h
+++ button.h
@@ -9,33 +9,48 @@ class Button{
 
 public:
 
+    /* Initialize the private variables */
     Button();
 
+    /* Set the size passed as parameter */
     void setSize(float x, float y);
     void setSize(sf::Vector2f size);
 
+    /* The return indicates if the button is
+    clicked or not*/
     bool isClicked();
 
+    /* Set the position of the button */
     void setPosition(float x, float y);
     void setPosition(sf::Vector2f position);
-    /*
-     * Return true if the button has been clicked,
-    */
+    
+    /* Return true if the button has been clicked */
     bool hasBeenClicked();
 
+    //TODO implement this
     float timeSinceLastClick();
 
+    /* Set the text on the button */
     void setText(std::string s);
+    
+    /* Set the text on the button and scale the button 
+    so the text fits on it */
     void setTextResize(std::string s);
 
+    //TODO this
     void enableClickEffect();
     void disableClickEffect();
 
+    /* Set the Texture that will be used as default button image */
     void setTexture(std::string name);
+    
+    /* Set the Texture that will be used as default button pressed image */
     void setPressedTexture(std::string name);
 
+    /* Draw the button on the window passed as parameter */
     void draw(sf::RenderWindow &w);
 
+    /* Update the private variables if needed acordingly to the event*/
     void handleEvent(sf::Event e);
 
 private:
